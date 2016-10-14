48244842dc955538b2ec414e19985bbd5368c10c
ak490216135 <490216135@qq.com>
2016-10-14 18:22:51 +0800

    更新抓取功能

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,31 +6,48 @@ class Index extends Controller
 {
     public function index()
     {
-        $a = file_get_contents('http://jandan.net/pic');
+
+        $url = 'http://jandan.net/pic';
+
+        $a = file_get_contents($url);
 
         $b = explode('<!-- begin comments -->', $a);
 
         $c = explode('<!-- end comments -->', $b[1]);
 
-        $d = explode('class="text">', $c[0]);
+        $d = explode('<li id="commen', $c[0]);
 
-        $str = preg_replace("/[\t\n\r]+/","",$d);
+        $fullstr = preg_replace("/[\t\n\r]+/","",$d);
 
-        // $str = substr(' ', $str);
+        array_shift($fullstr);
 
-        foreach ($str as $value) {
-        	echo $value;
+        $res = [];
+
+        foreach ($fullstr as $key => $value) {
+
+	        $e = explode('" target="_blank" class="view_img_link">', $value);
+	        $f = explode('</a></span><p>', $e[0]);
+	        $g = explode('<a href="', $f[1]);
+	        $res[$key]['jd_source'] = $g[1];
+
+	        $f2 = explode('<span class="righttext"><a href="', $value);
+	        $f2 = explode('">', $f2[1], 2);
+	        $res[$key]['jd_url'] = $f2[0];
+
+	        $f22 = explode('</a></span><p>', $f2[1]);
+	       	$res[$key]['jd_comment_num'] = $f22[0];
+
+	        $f3 = explode('[查看原图]</a><br /><img src="', $e[1]);
+	        $f3 = explode('" ', $f3[1], 2);
+	        $res[$key]['jd_thumb'] = $f3[0];
+
+	        $f4 = explode('</strong>', $value);
+	        $f4 = explode('" >', $f4[0]);
+	        $res[$key]['jd_author'] = $f4[1];
+
         }
-        //var_dump($str);
 
-/*        // 名称
-        $z['author'];
-        // 防伪码
-        $z['fwm'];
-        // oo
-        $z['oo'];
-        // xx
-        $z['xx'];*/
+        print_R($res);
 
         // return $this->fetch('index');
     }
